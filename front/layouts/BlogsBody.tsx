d9ba38ce38969a609c908a2bcca0d9112ddc5ec3
ISLEMNEDDAR <islam.neddar.pro@gmail.com>
2022-11-27 06:25:06 +0100

    update front

---
+++ front/layouts/BlogsBody.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BlogsBody() {
+    return (
+        <div className={'px-10'}>
+            Blogs body
+        </div>
+    );
+}
+
+export default BlogsBody;
---
+++ front/layouts/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <div className={'h-20 bg-primary px-10'}>
+            fdssdfs
+        </div>
+    );
+}
+
+export default NavBar;
