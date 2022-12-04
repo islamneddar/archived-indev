e435729c872bcc4daa3d039d6b39f2193f3829be
ISLEMNEDDAR <islam.neddar.pro@gmail.com>
2022-12-04 03:53:40 +0100

    update find way to add db entity

--- back/src/database/db.config.ts
+++ back/src/database/db.config.ts
@@ -3,19 +3,39 @@ import {SourceBlogEntity} from "../bussiness/source_blog/source_blog.entity";
 import {BlogEntity} from "../bussiness/blog/blog.entity";
 import {FeedBlogEntity} from "../bussiness/feed_blog/feed_blog.entity";
 import {TagEntity} from "../bussiness/tag/tag.entity";
+import {Logger} from "@nestjs/common";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import * as fs from "fs";
 
-export const DatabaseOrm = TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: '127.0.0.1',
-    port: 3306,
-    username: 'root',
-    password: '12345678',
-    database: 'indev_staging',
-    entities: [SourceBlogEntity,
-        BlogEntity,
-        FeedBlogEntity,
-        TagEntity
-    ],
-    synchronize: false,
-    logging : false
+const LOG = new Logger("db.config")
+
+const rdsCa = fs.readFileSync(`${__dirname}/../../ca-certificate.crt`);
+
+const DBModule = TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService) => {
+        return {
+            type: 'mysql',
+            host: process.env.DB_HOST || "",
+            port: Number(process.env.DB_PORT) || 33060,
+            username: process.env.DB_USERNAME || "",
+            password: process.env.DB_PASSWORD || "",
+            database: process.env.DB_NAME || "",
+            entities: [SourceBlogEntity,
+                BlogEntity,
+                FeedBlogEntity,
+                TagEntity
+            ],
+            synchronize: false,
+            logging: false,
+            ssl : {
+                ca : [rdsCa],
+                rejectUnauthorized : true,
+                require: true,
+            },
+        }
+    },
+    inject : [ConfigService]
 })
+
+export default DBModule;
