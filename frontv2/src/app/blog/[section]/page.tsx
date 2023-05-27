9681f62918d55cd3d8ed5defb31b15bddf7286cc
islam <islam.neddar@gmail.com>
2023-05-27 23:38:41 +0200

    add new page

--- frontv2/src/app/blog/[section]/page.tsx
+++ frontv2/src/app/blog/[section]/page.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import routing from '@/routes/routing.constant';
 import BlogsBody from '@/app-page-component/blog/BlogsBody';
-import {TypeFeed} from '@/types/api/source_blog';
 import SourceBlogBody from '@/app-page-component/source_blog/SourceBlogBody';
 import {useUserSessionSelector} from '@/redux/auth/user/user.selector';
 
