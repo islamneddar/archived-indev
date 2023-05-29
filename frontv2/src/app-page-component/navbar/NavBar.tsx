b5165cd5985f7d4d15fe2abd5f42d1d25252088e
islam <islam.neddar@gmail.com>
2023-05-29 23:13:13 +0200

    add responsive for mobiles

--- frontv2/src/app-page-component/navbar/NavBar.tsx
+++ frontv2/src/app-page-component/navbar/NavBar.tsx
@@ -8,22 +8,21 @@ function NavBar() {
   const router = useRouter();
 
   return (
-    <div className="h-24 bg-primary px-10 flex justify-between">
+    <div className="h-24 bg-primary sm:px-3 md:px-5 flex justify-between">
       {
         // height : 96px
       }
+      <div className="md:hidden h-full sm:flex justify-center items-center">
+        <AiOutlineMenu
+          className="w-8 h-8 cursor-pointer"
+          onClick={() => {
+            //dispatch(toggleSideBarTopic());
+          }}
+        />
+      </div>
       <div className="flex flex-row justify-between md:items-center h-full">
         <div className="h-full flex justify-center items-center">
-          <Image src={logoInDev} alt="logo" className="h-16 w-16" />
-        </div>
-
-        <div className="md:hidden h-full flex justify-center">
-          <AiOutlineMenu
-            className="w-8 cursor-pointer"
-            onClick={() => {
-              //dispatch(toggleSideBarTopic());
-            }}
-          />
+          <Image src={logoInDev} alt="logo" className="h-12 w-12" />
         </div>
       </div>
       <AuthContainer></AuthContainer>
--- frontv2/src/app-page-component/navbar/auth/AuthContainer.tsx
+++ frontv2/src/app-page-component/navbar/auth/AuthContainer.tsx
@@ -1,34 +1,107 @@
 'use client';
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import PrimaryButton from '@/components/button/PrimaryButton';
 import routing from '@/routes/routing.constant';
 import {useRouter} from 'next/navigation';
 import {useUserSessionSelector} from '@/redux/auth/user/user.selector';
+import {UserCircleIcon} from '@heroicons/react/24/solid';
+import {Menu} from 'primereact/menu';
+import {MenuItem} from 'primereact/menuitem';
+import PrimeReact from 'primereact/api';
+
+PrimeReact.appendTo = 'self';
+PrimeReact.autoZIndex = true;
 function AuthContainer() {
   const router = useRouter();
   const {isAuthenticated} = useUserSessionSelector();
 
+  const [openUserRegisterModal, setOpenUserRegisterModal] =
+    useState<boolean>(false);
+  const items: MenuItem[] = [
+    {
+      label: 'Options',
+      items: [
+        {
+          label: 'Login',
+          icon: 'pi pi-refresh',
+          command: () => {
+            console.log('login');
+          },
+        },
+        {
+          label: 'Register',
+          icon: 'pi pi-times',
+          command: () => {
+            console.log('register');
+          },
+        },
+      ],
+    },
+  ];
+
+  useEffect(() => {
+    console.log('fdfdfsfs');
+  }, []);
+
   if (!isAuthenticated) {
     return (
-      <div className={'flex justify-center items-center gap-2'}>
-        <PrimaryButton
-          title={'login'}
-          loading={false}
-          disabled={false}
-          buttonClassName={'bg-transparent pointer-cursor'}
-          onClick={() => {
-            router.push(routing.auth.login);
-          }}
-        />
-        <PrimaryButton
-          title={'register'}
-          loading={false}
-          disabled={false}
-          onClick={() => {
-            router.push(routing.auth.signup);
-          }}
-        />
-      </div>
+      <>
+        <div className={'hidden md:flex justify-center items-center gap-2'}>
+          <PrimaryButton
+            title={'login'}
+            loading={false}
+            disabled={false}
+            buttonClassName={'bg-transparent pointer-cursor'}
+            onClick={() => {
+              router.push(routing.auth.login);
+            }}
+          />
+          <PrimaryButton
+            title={'register'}
+            loading={false}
+            disabled={false}
+            onClick={() => {
+              router.push(routing.auth.signup);
+            }}
+          />
+        </div>
+        <div
+          className={'md:hidden flex h-full justify-center items-center mr-2'}>
+          <UserCircleIcon
+            className="h-12 w-12 text-gray-500 cursor-pointer"
+            onClick={() => {
+              setOpenUserRegisterModal(!openUserRegisterModal);
+            }}
+            aria-controls="popup_menu_right"
+            aria-haspopup
+          />
+          <div
+            className={` ${
+              openUserRegisterModal ? 'absolute' : 'hidden'
+            } top-20 right-2 w-32 bg-gray-400 z-50 rounded-xl`}>
+            <div>
+              <PrimaryButton
+                title={'login'}
+                loading={false}
+                disabled={false}
+                buttonClassName={'bg-transparent pointer-cursor w-full'}
+                onClick={() => {
+                  router.push(routing.auth.login);
+                }}
+              />
+              <PrimaryButton
+                title={'register'}
+                loading={false}
+                disabled={false}
+                buttonClassName={'bg-transparent pointer-cursor w-full'}
+                onClick={() => {
+                  router.push(routing.auth.signup);
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </>
     );
   }
 
