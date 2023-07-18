57213021280bfd4e4791064dbaeaf6bde27ba213
islam <islam.neddar@gmail.com>
2023-07-19 01:32:52 +0200

    add the search to  the prod

--- frontv2/src/app/layout.tsx
+++ frontv2/src/app/layout.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, {useEffect} from 'react';
 import './globals.css';
 import {Provider} from 'react-redux';
 import {store} from '@/redux/store';
@@ -13,6 +13,9 @@ import 'primereact/resources/themes/lara-light-indigo/theme.css';
 import 'primereact/resources/primereact.min.css';
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
+  useEffect(() => {
+    const tags = localStorage.getItem('tags');
+  }, []);
   return (
     <html lang="en">
       <head>
