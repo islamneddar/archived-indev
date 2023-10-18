aff521d7b6809bf61d2f5c01d38e9ceda24517e4
Islam Neddar <islam.neddar@gmail.com>
2023-10-18 14:13:06 +0200

    add the headless to true

--- back/src/external-services/screenshot-service/screenshot.service.ts
+++ back/src/external-services/screenshot-service/screenshot.service.ts
@@ -4,7 +4,9 @@ import puppeteer from 'puppeteer';
 @Injectable()
 export class ScreenshotService {
   async getScreenShot(url: string) {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      headless: true,
+    });
     const page = await browser.newPage();
 
     await page.goto(url, {waitUntil: 'networkidle0'});
