2b0b4aca350b418a84f919c55f11f82fc0357bd2
Islam Neddar <islam.neddar@gmail.com>
2023-10-19 16:25:23 +0200

    add default naviagion timeout to 1 min

--- back/src/external-services/screenshot-service/screenshot.service.ts
+++ back/src/external-services/screenshot-service/screenshot.service.ts
@@ -13,11 +13,16 @@ export class ScreenshotService {
 
     await page.setViewport({width: 1366, height: 768});
 
+    page.setDefaultNavigationTimeout(60000);
+
     LOG.debug('go to url', url);
     await page.goto(url, {
       waitUntil: 'networkidle2',
     });
+
     const screenshot = await page.screenshot();
+
+    await page.close();
 
     await browser.close();
     LOG.debug('----------- browser closed -----------');
