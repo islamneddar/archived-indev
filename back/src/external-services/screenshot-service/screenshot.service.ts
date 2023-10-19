1946832445ebef284d2df97a6fc1cb19d74c4f06
Islam Neddar <islam.neddar@gmail.com>
2023-10-19 02:22:28 +0200

    update parameters of puppeter

--- back/src/external-services/screenshot-service/screenshot.service.ts
+++ back/src/external-services/screenshot-service/screenshot.service.ts
@@ -1,8 +1,10 @@
 import {Injectable} from '@nestjs/common';
 import puppeteer from 'puppeteer';
+import Logger from '@/utils/logger';
 
 @Injectable()
 export class ScreenshotService {
+  LOG = new Logger(ScreenshotService.name);
   async getScreenShot(url: string) {
     const browser = await puppeteer.launch({
       headless: 'new',
@@ -10,11 +12,12 @@ export class ScreenshotService {
     });
     const page = await browser.newPage();
 
+    this.LOG.debug('go to url', url);
     await page.goto(url, {waitUntil: 'networkidle0'});
     const screenshot = await page.screenshot();
 
     await browser.close();
-
+    this.LOG.debug('----------- browser closed -----------');
     return screenshot;
   }
 }
--- back/src/utils/logger.ts
+++ back/src/utils/logger.ts
@@ -1,14 +1,14 @@
-import { configure, getLogger } from 'log4js';
+import {configure, getLogger} from 'log4js';
 
 configure({
   appenders: {
     console: {
       type: 'console',
-      layout: { type: 'pattern', pattern: '%d - %f : %[[%p]%] %l : %m' },
+      layout: {type: 'pattern', pattern: '%d - %f : %[[%p]%] %l : %m'},
     },
   },
   categories: {
-    default: { appenders: ['console'], level: 'trace', enableCallStack: true },
+    default: {appenders: ['console'], level: 'trace', enableCallStack: true},
   },
 });
 const LOG = getLogger();
